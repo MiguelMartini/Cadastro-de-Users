96695fc4ce05b077c3bf92dac091294e12ab595e
Miguel Martini <miguelkaulingm21@gmail.com>
2024-02-10 22:12:19 -0300

    adding cryptography on passwords

--- app.js
+++ app.js
@@ -1,44 +1,60 @@
 const express = require("express");
 const app = express();
-const handlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-const User = require('./models/User')
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+const bcrypt = require("bcrypt");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //config
-app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // body parser
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get("/home", function(req, res){
-        res.render('home', {
-            style: 'styleHome.css',
-        })
-})
+const saltRounds = 10;
 
-app.get("/form", function(req, res){
-    res.render('form', {
-        style: 'styleForm.css'
-    })
-})
-app.post("/add", function(req, res){
-    User.create({
+app.get("/home", function (req, res) {
+  res.render("home", {
+    style: "styleHome.css",
+  });
+});
+
+app.get("/register", function (req, res) {
+  res.render("cad", {
+    style: "styleForm.css",
+  });
+});
+
+
+app.get("/login", function (req, res) {
+  res.render("login", {
+    style: "styleLogin.css",
+  });
+});
+  
+app.post("/add", async (req, res) => {
+    try {
+      const hashedPass = await bcrypt.hash(req.body.password, saltRounds);
+  
+      await User.create({
         name: req.body.nome,
         email: req.body.email,
         fone: req.body.fone,
         cep: req.body.cep,
-        password: req.body.password
-    }).then(function(){
-        res.redirect('/home')
-    }).catch(function(err){
-        console.log(`Erro ${err}`)
-    })
-})
+        password: hashedPass,
+      });
+  
+      return res.redirect("/login");
+    } catch (err) {
+      console.error(`Erro ao criar usuário: ${err}`);
+      return res.status(500).send("Erro interno no servidor");
+    }
+  });
 
-app.listen(8081, function(){
-    console.log("Servidor rodando na url http://localhost:8081")
-})
+app.listen(8081, function () {
+  console.log("Servidor rodando na url http://localhost:8081/register");
+});
